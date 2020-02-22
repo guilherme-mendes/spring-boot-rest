59f13bd47f44666c5e9b96bccd81e1adbd05e29e
Guilherme Mendes <guimendesp12@gmail.com>
2020-02-22 18:24:51 -0300

    Create trade services

--- src/main/java/com/cabal/challenge/service/CommerceAddressService.java
+++ src/main/java/com/cabal/challenge/service/CommerceAddressService.java
@@ -1,4 +1,45 @@
 package com.cabal.challenge.service;
 
+import com.cabal.challenge.model.Commerce;
+import com.cabal.challenge.model.CommerceAddress;
+import com.cabal.challenge.repository.CommerceAddressRepository;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CommerceAddressService {
+
+    private final CommerceAddressRepository CommerceAddressRepository;
+
+    public CommerceAddressService(CommerceAddressRepository CommerceAddressRepository) {
+        this.CommerceAddressRepository = CommerceAddressRepository;
+    }
+
+    public CommerceAddress saveAddress(CommerceAddress CommerceAddress) {
+        return CommerceAddressRepository.save(CommerceAddress);
+    }
+
+    public List<CommerceAddress> getAddresses() {
+        return CommerceAddressRepository.findAll();
+    }
+
+    public CommerceAddress getAddressById(Long id) {
+        return CommerceAddressRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Address not found: " + id));
+    }
+
+    public CommerceAddress updateAddress(CommerceAddress CommerceAddress, Long id) {
+        CommerceAddress CommerceAddressToUpdate = getAddressById(id);
+        CommerceAddressToUpdate.setCommerce(new Commerce(CommerceAddress.getCommerce().getId()));
+        CommerceAddressToUpdate.setCep(CommerceAddress.getCep());
+        CommerceAddressToUpdate.setPublicPlace(CommerceAddress.getPublicPlace());
+        CommerceAddressToUpdate.setDistrict(CommerceAddress.getDistrict());
+        CommerceAddressToUpdate.setCity(CommerceAddress.getCity());
+        CommerceAddressToUpdate.setUf(CommerceAddress.getUf());
+        CommerceAddressToUpdate.setAdditionalAddress(CommerceAddress.getAdditionalAddress());
+
+        return saveAddress(CommerceAddressToUpdate);
+    }
 }
--- src/main/java/com/cabal/challenge/service/CommerceService.java
+++ src/main/java/com/cabal/challenge/service/CommerceService.java
@@ -1,4 +1,47 @@
 package com.cabal.challenge.service;
 
+import com.cabal.challenge.model.Commerce;
+import com.cabal.challenge.repository.CommerceRepository;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CommerceService {
+
+    private final CommerceRepository commerceRepository;
+
+    public CommerceService(CommerceRepository commerceRepository) {
+        this.commerceRepository = commerceRepository;
+    }
+
+    public Commerce saveCommerce(Commerce commerce) {
+        return commerceRepository.save(commerce);
+    }
+
+    public List<Commerce> getCommerces() {
+        return commerceRepository.findAll();
+    }
+
+    public Commerce getCommerceById(Long id) {
+        return commerceRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Commerce not found: " + id));
+    }
+
+    public Commerce deleteCommerce(Long id) {
+        Commerce commerceToDelete = getCommerceById(id);
+        commerceRepository.delete(commerceToDelete);
+
+        return commerceToDelete;
+    }
+
+    public Commerce updateCommerce(Commerce commerce, Long id) {
+        Commerce commerceToUpdate = getCommerceById(id);
+        commerceToUpdate.setName(commerce.getName());
+        commerceToUpdate.setCnpj(commerce.getCnpj());
+
+        return saveCommerce(commerceToUpdate);
+    }
+
 }
